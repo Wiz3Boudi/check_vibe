ff28d9b6837289389f76b67fdc1cfa5dbc3c5f81
Abdalah <wiz3bodi@gmail.com>
2026-09-01 13:22:55 -0700

    working on profile page

---
+++ src/data/profile.js
@@ -0,0 +1,17 @@
+export const profile = {
+  name: "Alex Rivera",
+  tag: "Digital creator. Finding vibes & good times. ✨",
+  username: "@arivera_vibes",
+  avatarURL: "./profile/avatarURL.jpg",
+  postsCount: 342,
+  followersCount: 12500,
+  following: 489,
+  images: [
+    "./profile/breakfast.jpg",
+    "./profile/sunset.jpg",
+    "./profile/out.jpg",
+    "./profile/building.jpg",
+    "./profile/skiing.jpg",
+    "./profile/chart.jpg",
+  ],
+};
