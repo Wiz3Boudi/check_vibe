5e0aece8f92919d92cbf9657d387024bc85dcde2
Abdalah <wiz3bodi@gmail.com>
2026-09-03 02:13:54 -0700

    category

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 import { profile } from "../data/profile";
-import { Share, Pen, Grid, Plus } from "lucide-react";
+import { Share, Pen, Plus, Grid3X3, Film, Bookmark } from "lucide-react";
+import { useState } from "react";
 
 export default function Profile() {
+  const Icons = [
+    { id: "grid", icon: Grid3X3 },
+    { id: "film", icon: Film },
+    { id: "bookmark", icon: Bookmark },
+  ];
+  const [category, setCategory] = useState("grid");
+
   return (
     <Container>
       <Avatar>
@@ -28,28 +36,53 @@ export default function Profile() {
       </Conntention>
       <ProfileOptions>
         <button>
-          <Pen size={15} /> <span>Edit Profile</span>
+          <Pen size={20} /> <span>Edit Profile</span>
         </button>
         <button>
-          <Share size={15} /> <span>Share Profile</span>
+          <Share size={20} /> <span>Share Profile</span>
         </button>
       </ProfileOptions>
       <Posts>
-        <div>
-          <div>
-            <Grid />
-          </div>
-          <div>
-            <Grid />
-          </div>
-          <div>
-            <Grid />
-          </div>
+        <div className="categories">
+          {Icons?.map((Item) => {
+            return (
+              <button
+                key={Item.id}
+                className={Item.id === category ? "active" : ""}
+                onClick={() => setCategory(Item.id)}
+              >
+                {<Item.icon />}
+              </button>
+            );
+          })}
         </div>
-        <div>
-          {profile.images.map((image, index) => (
-            <img key={index} src={image} alt="post -  iamges" />
-          ))}
+        <div className="images">
+          {category === "grid" &&
+            profile.images.map((img, index) => {
+              return (
+                <Post key={index}>
+                  <img src={img} alt={img} />
+                </Post>
+              );
+            })}
+          {category === "film" &&
+            profile.images
+              .slice(4, profile.images.length - 1)
+              .map((img, index) => {
+                return (
+                  <Post key={index}>
+                    <img src={img} alt={img} />
+                  </Post>
+                );
+              })}
+          {category === "bookmark" &&
+            profile.images.slice(1, 3).map((img, index) => {
+              return (
+                <Post key={index}>
+                  <img src={img} alt={img} />
+                </Post>
+              );
+            })}
         </div>
       </Posts>
     </Container>
@@ -59,11 +92,8 @@ export default function Profile() {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1rem;
-  img {
-    width: 100px;
-    height: 100px;
-  }
+  align-items: center;
+  gap: 2rem;
 `;
 const Avatar = styled.div`
   position: relative;
@@ -101,9 +131,13 @@ const UserInfo = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 5px;
+  p {
+    color: var(--text-muted);
+  }
   p:first-child {
     font-size: 1.9rem;
     font-weight: 600;
+    color: black;
   }
   button {
     color: var(--primary-container);
@@ -112,9 +146,10 @@ const UserInfo = styled.div`
   }
 `;
 const Conntention = styled.div`
+  align-self: center;
   display: flex;
   justify-content: space-around;
-  align-items: center;
+  width: 80%;
   div {
     display: flex;
     flex-direction: column;
@@ -123,6 +158,7 @@ const Conntention = styled.div`
     p {
       font-weight: 500;
       font-size: 0.8rem;
+      color: var(--text-muted);
     }
     p:first-child {
       weight: 600;
@@ -132,16 +168,57 @@ const Conntention = styled.div`
   }
 `;
 const ProfileOptions = styled.div`
+  align-self: center;
   display: flex;
   justify-content: space-around;
   align-items: center;
+  width: 90%;
   button {
     display: flex;
     jsutify-content: cneter;
     align-items: center;
     gap: 10px;
-    padding: 10px;
+    padding: 10px 15px;
+    border-radius: 20px;
+    border: 1px solid var(--primary-fixed-dim);
+    background-color: white;
+    color: var(--primary);
+    curosr: pointer;
+    font-weight: 600;
+    font-size: 1rem;
   }
 `;
-const Posts = styled.div``;
-const Post = styled.div``;
+const Posts = styled.div`
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 0 1rem;
+  .categories {
+    display: flex;
+    align-items: cneter;
+    justify-content: space-around;
+    button {
+      width: 100%;
+      background: none;
+      padding: 5px 0;
+      border-bottom: 3px solid transparent;
+      cursor: pointer;
+    }
+    .active {
+      border-bottom: 3px solid var(--primary);
+    }
+  }
+  .images {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: auto;
+    column-gap: 3px;
+  }
+`;
+const Post = styled.div`
+  img {
+    width: 100%;
+    height: 150px;
+  }
+`;
