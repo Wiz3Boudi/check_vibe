94a447fb538a4b3f42a0b3a4d008ea97198d1bbb
Abdalah <wiz3bodi@gmail.com>
2026-09-09 14:35:14 -0700

    last

--- src/componants/ViewProfile.jsx
+++ src/componants/ViewProfile.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import { X, Share } from "lucide-react";
-import { useEffect } from "react";
+import { Share, X } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Icons } from "../data/profile";
 
 export default function ViewProfile({ data, showProfileToggle, showProfile }) {
+  const [activeCategroy, setActiveCategrory] = useState("gird");
+
   useEffect(() => {
     document.body.style.overflow = showProfile ? "hidden" : "unset";
     return () => {
@@ -22,23 +25,40 @@ export default function ViewProfile({ data, showProfileToggle, showProfile }) {
             <Name>{data.username}</Name>
             <Bio> {data.bio} </Bio>
             <Username> {`@${data.username}`} </Username>
-            <ProfileConnection>
-              {data.connectingInfo.map((p) => (
-                <Info key={p.id}>
-                  <h4> {p.count} </h4>
-                  <p> {p.text} </p>
-                </Info>
-              ))}
-            </ProfileConnection>
-            <Section>
-              <button aria-label="follow"> Follow </button>
-              <button aria-label="share">
-                {" "}
-                <Share /> Share{" "}
-              </button>
-            </Section>
           </Avatar>
         </Header>
+        <ProfileConnection>
+          {data.connectingInfo.map((p) => (
+            <Info key={p.id}>
+              <h4> {p.count} </h4>
+              <p> {p.text} </p>
+            </Info>
+          ))}
+        </ProfileConnection>
+        <Section>
+          <button aria-label="follow">
+            {data.isFollowing ? "Following" : "Follow"}
+          </button>
+          <button aria-label="share">
+            <Share /> Share
+          </button>
+        </Section>
+        <PostsContainer>
+          <PostsBar>
+            {Icons.map(({ id, icon }) => {
+              const Icon = icon;
+              return (
+                <Category
+                  key={id}
+                  onClick={() => setActiveCategrory(id)}
+                  className={id === activeCategroy ? "active" : ""}
+                >
+                  <Icon />
+                </Category>
+              );
+            })}
+          </PostsBar>
+        </PostsContainer>
       </Wrapper>
     </Container>
   );
@@ -48,26 +68,80 @@ const Container = styled.div`
   inset: 0;
   z-index: 2000;
   background-color: rgba(0, 0, 0, 0.4);
-  padding: 10px;
+  padding: 5px;
 `;
 const Wrapper = styled.div`
   background-color: white;
   height: 100%;
   border-radius: 10px;
-  padding: 5px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `;
-const Close = styled.button``;
-const Header = styled.div``;
-const Avatar = styled.div``;
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+const Close = styled.button`
+  display: flex;
+  width: fit-content;
+  border-radius: 50%;
+  padding: 3px;
+  cursor: pointer;
+  background: none;
+  &:hover {
+    background: revert;
+  }
+`;
+const Avatar = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
 const AvatarImg = styled.img`
-  width: 100px;
+  width: 150px;
   aspect-ratio: 1/1;
   object-fit: cocer;
   border-radius: 50%;
+  curosr: pointer;
 `;
 const Name = styled.h3``;
 const Bio = styled.p``;
 const Username = styled.h4``;
-const ProfileConnection = styled.div``;
-const Info = styled.div``;
+const ProfileConnection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+const Info = styled.div`
+  diplay: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
 const Section = styled.div``;
+const PostsContainer = styled.div``;
+const PostsBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .active {
+    border-bottom: 3px solid var(--primary);
+  }
+`;
+const Category = styled.button`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: none;
+  padding: 5px 0;
+  border-bottom: 3px solid transparent;
+  svg {
+    cursor: pointer;
+  }
+`;
+const Posts = styled.div``;
+const Post = styled.div``;
